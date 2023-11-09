47577c3ecf0d7879ad5e54bbfcf2876d77710e70
chrisjwalker <8091191+chrisjwwalker@users.noreply.github.com>
2023-11-09 08:56:58 +0000

    Initial project

---
+++ CleanSwiftUI/CleanSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanSwiftUIApp.swift
+//  CleanSwiftUI
+//
+//  Created by Chris J W Walker on 09/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct CleanSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
